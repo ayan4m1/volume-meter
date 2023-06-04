f1c415a1a7131001f7b8ac4fd4b7d327eefebfad
ayan4m1 <andrew@bulletlogic.com>
2023-06-04 14:49:49 -0400

    wip

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+
+#include <Audio.hpp>
+
+TFT_eSPI lcd = TFT_eSPI();
+Audio audio = Audio();
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+  delay(100);
+  Serial.println("Awake");
+
+  audio.begin();
+
+  lcd.init();
+  lcd.setRotation(1);
+  lcd.fillScreen(TFT_BLACK);
+}
+
+void loop() {
+  double db = audio.getDecibels();
+
+  if (db > 0) {
+    Serial.printf("%.1f\n", db);
+  }
+
+  delay(500);
+}
